617a6c4b95e2aac32256c489a4a574bcf2fabec5
aylton <ayltondias703@gmail.com>
2024-08-18 23:41:19 -0300

    getId/put

--- api/api.js
+++ api/api.js
@@ -5,12 +5,19 @@ const Filme = require("../filmes/Filme");
 
 router.get("/filmes", (req, res) => {
     Filme.findAll().then( filmes => {
-        res.send("todos os filmes");
+        res.send("Todos os filmes");
+    });
+});
+
+router.get("/filme/:id", (req, res) => {
+    var id = req.params.id;
+    Filme.findByPk(id).then( filme => {
+        res.send("Filme desejado");
     });
 });
 
 router.post("/filme", (req, res) => {
-    var = {titulo, genero, estudio, diretor} = req.body;
+    var {titulo, genero, estudio, diretor} = req.body;
     Filme.create({titulo, genero, estudio, diretor}).then(() => {
         res.send("Filme criado");
     });
@@ -20,6 +27,14 @@ router.delete("/filme/:id", (req, res) => {
     var id = req.params.id;
     Filme.destroy({where: {id}}).then( () => {
         res.send("Filme deletado");
+    });
+});
+
+router.put("/filme/:id", (req, res) => {
+    var id = req.params.id;
+    var {titulo, genero, estudio, diretor} = req.body;
+    Filme.update({titulo, genero, estudio, diretor}, {where: {id}}).then(() => {
+        res.send("Filme atualizado");
     });
 });
 
