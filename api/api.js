866f0eb6532b6b0f459db7846e82854f576f7b71
aylton <ayltondias703@gmail.com>
2024-08-18 23:35:58 -0300

    get/create/delete

--- api/api.js
+++ api/api.js
@@ -3,4 +3,25 @@ const router = express.Router();
 const Filme = require("../filmes/Filme");
 
 
+router.get("/filmes", (req, res) => {
+    Filme.findAll().then( filmes => {
+        res.send("todos os filmes");
+    });
+});
+
+router.post("/filme", (req, res) => {
+    var = {titulo, genero, estudio, diretor} = req.body;
+    Filme.create({titulo, genero, estudio, diretor}).then(() => {
+        res.send("Filme criado");
+    });
+});
+
+router.delete("/filme/:id", (req, res) => {
+    var id = req.params.id;
+    Filme.destroy({where: {id}}).then( () => {
+        res.send("Filme deletado");
+    });
+});
+
+
 module.exports = router;
